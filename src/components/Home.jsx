7ef8fb4dae77693156afa4463d483a3153c98342
prudorexa <mathuprudence24@gmail.com>
2024-04-22 14:06:05 +0300

    add heading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,20 @@
 
 const Home = () => {
   return (
-    <h1 className="text-3xl font-bold text-centre text-red-500 mt=4 mt-5 mx-20 bg=blue-500 border border-gray-300 rounded=1g bg-gray-100">
-      Welcome to my soft_exp!
-    </h1>
-    
-
+    <div cl>
+      <h1 className="text-3xl font-bold text-centre text-red-500 mt=4 mt-5 mx-20 bg=blue-500 border border-gray-300 rounded=1g bg-gray-100">
+        Welcome to my soft_exp!
+      </h1>
+      <p className="mt-5 mx-7 mb-10"><img src="src/assets/expense.jpg" alt=" width-100%" /></p>
+      <h2 className="text-3xl font-bold text-centre text-red-500 mt=4 mt-5 mx-20 bg=blue-500"> Expense tracker Project</h2>
+      <p> The Expense Tracker project is a web application developed using React.
+          Its main purpose is to assist users in keeping track of their expenses.
+          With this app, users can easily add,
+          and delete expenses to view a summary of their spending habits as well and it will show the available balance the user has left.
+      </p>
+      
+     
+    </div>
   )
 
 }
