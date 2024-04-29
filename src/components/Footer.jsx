d5232649d4f8152af18cb3d34aa380fc68aa65be
prudorexa <mathuprudence24@gmail.com>
2024-04-29 12:59:54 +0300

    add heading

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,10 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
   const [emailAddress, setEmailAddress] = useState('');
+  const [error, setError] = useState('');
 
-  const handleSubscribe = () => {
+  const handleSubscribe = (e) => {
+    e.preventDefault();
     if (!emailAddress) {
       alert('Please enter your email');
       return;
@@ -14,6 +16,8 @@ const Footer = () => {
     console.log('Subscribing with email address:', emailAddress);
 
     setEmailAddress('');
+    setError('');
+alert('Thank you for subscribing!');
   };
 
   return (
@@ -24,21 +28,28 @@ const Footer = () => {
             <span className='text-teal-400'>Free</span> Welcome to Soft-exp
           </h1>
           <p className="text-gray-300 mb-4">Keep track of your expenses effortlessly with Soft-exp. Stay organized and manage your finances with ease.</p>
+          <form onSubmit={handleSubscribe}>
+
           <div className="flex items-center mb-4">
             <input 
-              type="text" 
+              type="email" 
+              id='email'
+              name='email'
               placeholder='Enter Your email' 
               className='text-gray-800 sm:w-72 w-full sm:mr-5 mr-1 lg:mb-0 py-2.5 rounded px-2 focus:outline-none' 
               value={emailAddress} 
               onChange={(e) => setEmailAddress(e.target.value)} 
             />
             <button 
+              type='submit'
               className='bg-teal-400 hover:bg-teal-500 duration-300 px-5 py-2.5 font-[popins] rounded-md text-white md:w-auto w-full'
               onClick={handleSubscribe}
             >
               Subscribe
             </button>
           </div>
+          {error && <p className="text-red-500">{error}</p>} 
+          </form>
         </div>
         <div className="md:w-3/5">
           <ul className="md:flex md:justify-end space-y-2 md:space-y-0 md:space-x-6">
