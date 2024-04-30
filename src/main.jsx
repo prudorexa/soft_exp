d1bcb8cc205e420a94dbc43e1c9a87af06d6cdfb
prudorexa <mathuprudence24@gmail.com>
2024-04-30 13:40:05 +0300

    modify vercel.json file

--- src/main.jsx
+++ src/main.jsx
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId='564943998574-odb4d7kt21esl1k3i8fs2garkjsdh5f4.apps.googleusercontent.com'>
     <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
